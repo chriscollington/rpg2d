26d322c1569fb2a0260f750888cb088c38f94d97
chris <540218@escg.ac.uk>
2024-11-03 23:02:48 +0000

    home

---
+++ Assets/scripts/OgreScene.cs
@@ -0,0 +1,9 @@
+//using UnityEngine;
+
+//public class OgreScene : MonoBehaviour
+{
+    //void OnTriggerEnter2D(Collider2D other)
+    {
+
+    }
+}
---
+++ Assets/scripts/SceneSwitch.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(7);
+    }
+}
